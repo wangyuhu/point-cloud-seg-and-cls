80844f9ff47d69cbcb5c74473024e9d215d0b488
wangyuhu <2956024283@qq.com>
2020-04-01 22:16:24 +0800

    路径等基本设置私人化

--- pointSIFT/tf_utils/tf_ops/pointSIFT_op/tf_pointSIFT_compile.sh
+++ pointSIFT/tf_utils/tf_ops/pointSIFT_op/tf_pointSIFT_compile.sh
@@ -2,4 +2,4 @@
 /usr/local/cuda-8.0/bin/nvcc pointSIFT.cu -o pointSIFT_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 # TF1.4
-g++ -std=c++11 main.cpp pointSIFT_g.cu.o -o tf_pointSIFT_so.so -shared -fPIC -I /home/jmydurant/anaconda3/envs/pointsift/lib/python3.5/site-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /home/jmydurant/anaconda3/envs/pointsift/lib/python3.5/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/home/jmydurant/anaconda3/envs/pointsift/lib/python3.5/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 main.cpp pointSIFT_g.cu.o -o tf_pointSIFT_so.so -shared -fPIC -I /home/wangyuhu/anaconda2/envs/py35-tf141/lib/python3.5/site-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /home/wangyuhu/anaconda2/envs/py35-tf141/lib/python3.5/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/home/wangyuhu/anaconda2/envs/py35-tf141/lib/python3.5/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
